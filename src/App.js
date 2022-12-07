a8f26ad17f732fd2d20577529f4866e0b221fd06
AndreiWWW <andreiv.banut@gmail.com>
2022-12-07 17:53:36 +0200

    added base files, created Button component, added prop-types library, added conditions to button & added tailwind css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+function App() {
+    return <div>
+        <div><Button>Click me!</Button></div>
+        <div><Button>Bye bye!</Button></div>
+        <div><Button>Click bait!</Button></div>
+        <div><Button>Click him!</Button></div>
+        <div><Button>Click her!</Button></div>
+        <div><Button>Click us!</Button></div>
+        
+        </div>;
+}
+
+export default App;
