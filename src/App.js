cd222131ad4122e12aab3cecd118b38c2e93fd91
AndreiWWW <andreiv.banut@gmail.com>
2022-12-12 23:44:35 +0200

    created DropDown page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
-import { useState } from 'react';
-import DropDown from "./components/DropDown";
-
 function App() {
-	const [selection, setSelection] = useState(null);
-
-	const handleSelect = (option) => {
-		setSelection(option);
-	}
-
-	const options = [
-		{label: 'Red', value: 'red'},
-		{label: 'Green', value: 'green'},
-		{label: 'Blue', value: 'blue'},
-	];
-
-	return (
-		<div className="flex">
-			<DropDown options={options} value={selection} onChange={handleSelect} />
-		</div>
-	);
+	return <div>DRPdown</div>
 	
 }
 export default App;
